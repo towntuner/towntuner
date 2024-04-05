d365bc3322bfb06512157acf173119736a5df2d4
Sönke Peters <soenke@soenkep.com>
2024-04-05 21:10:57 +0200

    feat: complete survey form

--- src/pages/app/new.tsx
+++ src/pages/app/new.tsx
@@ -10,6 +10,9 @@ export const getServerSideProps: GetServerSideProps = async () => {
   const campaignId = nanoid();
   await store.setJSON(campaignId, {
     createdAt: new Date().toISOString(),
+    title: "",
+    description: "",
+    icon: "💬",
   });
 
   return {
