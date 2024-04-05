f5da5ae411184fb01e64507ed76cd0876b55657d
Florian <floryus2004@gmail.com>
2024-04-05 18:48:54 +0200

    Refactor app/index.tsx to fetch campaigns from the server

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -2,10 +2,14 @@
 import Header from "@/components/Header";
 import { RiArrowRightUpLine } from "@remixicon/react";
 import { Card, Divider } from "@tremor/react";
+import { getStore } from "@netlify/blobs";
+import { useEffect } from "react";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
-interface ProjectProps {
+interface CampaignProps {
   title: string;
   icons: string;
+  color: string;
   href: string;
 }
 
@@ -13,66 +17,22 @@ function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-const data = [
-  {
-    name: "Alissia Stone",
-    initials: "AS",
-    email: "a.stone@gmail.com",
-    textColor: "text-fuchsia-800 dark:text-fuchsia-500",
-    bgColor: "bg-fuchsia-100 dark:bg-fuchsia-500/20",
-    href: "#",
-  },
-  {
-    name: "Emma Bern",
-    initials: "EB",
-    email: "e.bern@gmail.com",
-    textColor: "text-blue-800 dark:text-blue-500",
-    bgColor: "bg-blue-100 dark:bg-blue-500/20",
-    href: "#",
-  },
-  {
-    name: "Aaron McFlow",
-    initials: "AM",
-    email: "a.flow@acme.com",
-    textColor: "text-pink-800 dark:text-pink-500",
-    bgColor: "bg-pink-100 dark:bg-pink-500/20",
-    href: "#",
-  },
-  {
-    name: "Thomas Palstein",
-    initials: "TP",
-    email: "t.palstein@acme.com",
-    textColor: "text-emerald-800 dark:text-emerald-500",
-    bgColor: "bg-emerald-100 dark:bg-emerald-500/20",
-    href: "#",
-  },
-  {
-    name: "Sarah Johnson",
-    initials: "SJ",
-    email: "s.johnson@gmail.com",
-    textColor: "text-orange-800 dark:text-orange-500",
-    bgColor: "bg-orange-100 dark:bg-orange-500/20",
-    href: "#",
-  },
-  {
-    name: "David Smith",
-    initials: "DS",
-    email: "d.smith@gmail.com",
-    textColor: "text-indigo-800 dark:text-indigo-500",
-    bgColor: "bg-indigo-100 dark:bg-indigo-500/20",
-    href: "#",
-  },
-  {
-    name: "Megan Brown",
-    initials: "MB",
-    email: "m.brown@gmail.com",
-    textColor: "text-yellow-800 dark:text-yellow-500",
-    bgColor: "bg-yellow-100 dark:bg-yellow-500/20",
-    href: "#",
-  },
-];
+export const getServerSideProps: GetServerSideProps = async () => {
+  const campaignsStore = getStore("campaigns");
+  const { blobs } = await campaignsStore.list();
+  let campaigns: any = [];
+  await Promise.all(
+    blobs.map(async (blob) => {
+      campaigns.push(await campaignsStore.get(blob.key, { type: "json" }));
+    })
+  );
+  console.log(campaigns);
+  return { props: { campaigns: blobs } };
+};
 
-export default function App() {
+export default function App({
+  campaigns,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <>
       <Header />
@@ -82,34 +42,30 @@ export default function App() {
             Projects
           </h3>
           <span className="inline-flex h-6 w-6 items-center justify-center rounded-tremor-full bg-tremor-background-subtle text-tremor-label font-medium text-tremor-content-strong dark:bg-dark-tremor-background-subtle dark:text-dark-tremor-content-strong">
-            {data.length}
+            {campaigns.length}
           </span>
         </div>
         <Divider className="my-4" />
         <div className="mt-4 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-          {data.map((member) => (
-            <Card key={member.name} className="group">
+          {campaigns.map((campaign: any) => (
+            <Card key={campaign.title} className="group">
               <div className="flex items-center space-x-4">
                 <span
                   className={classNames(
-                    member.bgColor,
-                    member.textColor,
+                    campaign.color,
                     "flex h-12 w-12 shrink-0 items-center justify-center rounded-full text-tremor-default font-medium"
                   )}
                   aria-hidden={true}
                 >
-                  {member.initials}
+                  {campaign.icons}
                 </span>
                 <div className="truncate">
                   <p className="truncate text-tremor-default font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">
-                    <a href={member.href} className="focus:outline-none">
+                    <a href={campaign.href} className="focus:outline-none">
                       {/* Extend link to entire card */}
                       <span className="absolute inset-0" aria-hidden={true} />
-                      {member.name}
+                      {campaign.title}
                     </a>
-                  </p>
-                  <p className="truncate text-tremor-default text-tremor-content dark:text-dark-tremor-content">
-                    {member.email}
                   </p>
                 </div>
               </div>
