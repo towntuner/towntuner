d12d1ce67192b326f06dfd77761c8042beee9127
Simon Knott <info@simonknott.de>
2024-04-06 01:54:12 +0200

    adobe express integration

--- src/components/socialsTab.tsx
+++ src/components/socialsTab.tsx
@@ -1,14 +1,64 @@
-import { RiInstagramFill } from "@remixicon/react";
+import { RiEdit2Fill, RiInstagramFill } from "@remixicon/react";
 import { Button } from "@tremor/react";
+import Script from "next/script";
+import { useState } from "react";
+
+function AdobeExpress(props: { campaignId: string }) {
+  return (
+    <Script
+      src="https://cc-embed.adobe.com/sdk/v4/CCEverywhere.js"
+      onLoad={async () => {
+        const ccEverywhere = await window.CCEverywhere.initialize(
+          {
+            clientId: process.env.NEXT_PUBLIC_ADOBE_EXPRESS_KEY,
+            appName: "towntuner",
+          },
+          {
+            locale: "de_DE",
+          }
+        );
+
+        const imageResponse = await fetch(
+          `/api/generate-social-image?campaignId=${props.campaignId}`
+        );
+        let string = "";
+        new Uint8Array(await imageResponse.arrayBuffer()).forEach((byte) => {
+          string += String.fromCharCode(byte);
+        });
+        string = btoa(string);
+
+        ccEverywhere.editor.create({}, {
+          callbacks: {
+            onPublish({ projectId }: { projectId: string }) {
+              // TODO: store this somewhere, so project can be re-opened later
+              console.log("Adobe Project ID: ", projectId)
+            }
+          }
+        });
+      }}
+    />
+  );
+}
 
 export function SocialsTab(props: { campaignId: string }) {
+  const [showExpress, setShowExpress] = useState(false);
   return (
-    <div>
+    <div className="flex space-x-4">
       <a href={`/api/generate-social-image?campaignId=${props.campaignId}`}>
         <Button icon={RiInstagramFill} size="xl" type="button">
           Instagram Ad
         </Button>
       </a>
+
+      <Button
+        icon={RiEdit2Fill}
+        size="xl"
+        type="button"
+        onClick={() => setShowExpress(true)}
+      >
+        Adobe Express öffnen
+      </Button>
+      {showExpress && <AdobeExpress campaignId={props.campaignId} />}
     </div>
   );
 }
